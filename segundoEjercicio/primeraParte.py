0bdf95cdd0810ee6ba56b82376379581fca12db9
Camila Lopez <camila_lopez_39@hotmail.com>
2021-07-07 23:52:54 +0200

    agregando funcion abrirArichivo() y comentarios informativos

--- segundoEjercicio/primeraParte.py
+++ segundoEjercicio/primeraParte.py
@@ -1,32 +1,36 @@
-nuevaLista= []
+def abrirArchivo():
+    nuevaLista= []
+    contraseñasGuardadas = open('segundoEjercicio/input.txt', 'r')
+    line = contraseñasGuardadas.readlines()
 
-contraseñasGuardadas = open('segundoEjercicio/input.txt', 'r')
-line = contraseñasGuardadas.readlines()
-
-for i in line:
-    valor = i.split()
-    rango= valor[0].split("-")
-    clave=valor[1].replace(":", "")
-    contraseña=valor[2]
-    nuevaLista.append([rango,clave,contraseña])
-    
-contraseñasGuardadas.close() 
+    #Ciclo para convertir el archivo txt en vector
+    for i in line:
+        valor = i.split()
+        rango= valor[0].split("-")
+        caracterPolitica=valor[1].replace(":", "")
+        contraseña=valor[2]
+        nuevaLista.append([rango,caracterPolitica,contraseña])
+        
+    contraseñasGuardadas.close() 
+    return nuevaLista
 
 def evaluarContraseña(listaContraseñas):
     contadorValidas = 0
     contadorInvalidas = 0
     for i in listaContraseñas:
-        minimo = i[0][0]
-        maximo = i[0][1]
-        clave = i[1]
-        repeticiones = i[2].count(clave)
-        if repeticiones>=int(minimo) and repeticiones<=int(maximo):
+        #identificando los argumentos numericos de la politica de la contraseña
+        minimo = int(i[0][0]) 
+        maximo = int(i[0][1]) 
+        caracterPolitica = i[1] #identifica caracter de la politica de la contraseña
+        repeticiones = i[2].count(caracterPolitica) #identifica repeticion de el caracter de la politica en contraseña
+        if repeticiones>=minimo and repeticiones<=maximo:
             contadorValidas+=1
         else:
             contadorInvalidas+=1
     return f'Numero de contraseñas validas: {contadorValidas} \nNumero de contraseñas invalidas: {contadorInvalidas}'
+
   
-print(evaluarContraseña(nuevaLista))
+print(evaluarContraseña(abrirArchivo()))
 
 
 
--- segundoEjercicio/segundaParte.py
+++ segundoEjercicio/segundaParte.py
@@ -1,40 +1,40 @@
-nuevaLista= []
+def abrirArchivo():
+    nuevaLista= []
+    contraseñasGuardadas = open('segundoEjercicio/input.txt', 'r')
+    line = contraseñasGuardadas.readlines()
+    #Ciclo para convertir en vector al archivo txt 
+    for i in line:
+        valor = i.split()   
+        posiciones= valor[0].split("-")
+        clave=valor[1].replace(":", "")
+        contraseña=valor[2]
+        nuevaLista.append([posiciones,clave,contraseña])
 
-contraseñasGuardadas = open('segundoEjercicio/input.txt', 'r')
-line = contraseñasGuardadas.readlines()
-
-for i in line:
-    valor = i.split()
-    posiciones= valor[0].split("-")
-    clave=valor[1].replace(":", "")
-    contraseña=valor[2]
-    nuevaLista.append([posiciones,clave,contraseña])
-
-contraseñasGuardadas.close() 
+    contraseñasGuardadas.close()
+    return nuevaLista 
 
 def evaluarContraseña(listaContraseñas):
     contadorValidas = 0
     contadorInvalidas = 0
     for i in listaContraseñas:
+        #identificando el valor de las posiciones de la politica de contraseña
         primeraPosicion = int(i[0][0]) 
         segundaPosicion = int(i[0][1]) 
-        clave = i[1]
+        caracterPolitica = i[1] 
         contraseña = i[-1]
-        if (contraseña[primeraPosicion-1]==clave) and (contraseña[segundaPosicion-1]==clave):
+        if (contraseña[primeraPosicion-1]==caracterPolitica) and (contraseña[segundaPosicion-1]==caracterPolitica):
             contadorInvalidas+=1
-        elif (contraseña[primeraPosicion-1]==clave) or (contraseña[segundaPosicion-1]==clave):
+        elif (contraseña[primeraPosicion-1]==caracterPolitica) or (contraseña[segundaPosicion-1]==caracterPolitica):
             contadorValidas+=1
         else:
             contadorInvalidas+=1
             
     return f'Numero de contraseñas validas: {contadorValidas} \nNumero de contraseñas invalidas: {contadorInvalidas}'
   
-print(evaluarContraseña(nuevaLista))
+print(evaluarContraseña(abrirArchivo()))
 
 
 
-        
-    
 
     
    
